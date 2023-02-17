4e8296e65d4e4af553e4f0f69eeb1be51dc580f8
Ankur Rawat <ankurrawat620@gmail.com>
2023-02-18 01:20:39 +0530

    Done Basic Express Calculator

--- calculator.js
+++ calculator.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    res.send("<h1>hi</h1>");
+    res.sendFile(__dirname+"/index.html");
 })
+
+app.post("/", (req, res) =>{
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+    const result = num1 + num2;
+    res.send("Result of that calculation is:  " + result);
+})
+
 
 app.listen("3000", () => {
     console.log("Server Listining at Port 3000");
